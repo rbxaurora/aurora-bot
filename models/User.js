4616bc40a564e22700d3d76ed0266af3a59b90e8
aurorateam <115458114+rbxaurora@users.noreply.github.com>
2023-09-03 09:35:58 +0300

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Schema, model } = require(`mongoose`);
 
 const userSchema = new Schema ({
@@ -26,33 +25,4 @@ const userSchema = new Schema ({
 const User = model('user', userSchema);
 
 
-=======
-const { Schema, model } = require(`mongoose`);
-
-const userSchema = new Schema ({
-	auroraID: {
-		type: Number,
-		required: true,
-		unique: true
-	},
-	name: {
-		type: String,
-		required: true
-	},
-	role: {
-		type: String,
-		required: true,
-		default: "USER" 
-	},
-	warns: {
-		type: Object
-	},
-	isAdmin: Boolean,
-	birthday: 'String'
-});
-
-const User = model('user', userSchema);
-
-
->>>>>>> 7fe6d55ff2fa1ca38214a0bc947977f95de2e5b1
 module.exports = User;
